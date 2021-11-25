8ecc87f11dab544474d7370649ca6e7d8d774aec
Daniel <sysmoon@sk.com>
2021-11-26 08:39:40 +0900

    update github url by github.com/sysmoon

--- simple-grpc-gateway/vps-grpc-gateway/main.go
+++ simple-grpc-gateway/vps-grpc-gateway/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 
-	userpb "github.com/dojinkimm/go-grpc-example/protos/vpsdata"
+	userpb "github.com/sysmoon/go-grpc-example/protos/vpsdata"
 )
 
 const (
