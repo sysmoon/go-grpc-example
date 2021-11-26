418dfae652588b281879ebd6a524bf6893b87745
Daniel <sysmoon@sk.com>
2021-11-26 10:54:22 +0900

    test

--- simple-grpc-gateway/vps-grpc-gateway/main.go
+++ simple-grpc-gateway/vps-grpc-gateway/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 
-	userpb "github.com/sysmoon/go-grpc-example/protos/v2/vpdata"
+	userpb "github.com/sysmoon/go-grpc-example/protos/vpdata"
 )
 
 const (
