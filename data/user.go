56978ff2bb70ba293fcb788a1ae3606a060ad9af
Daniel <sysmoon@sk.com>
2021-11-29 10:17:17 +0900

    test

--- data/user.go
+++ data/user.go
@@ -1,7 +1,7 @@
 package data
 
 import (
-	userpb "github.com/dojinkimm/go-grpc-example/protos/v1/user"
+	userpb "github.com/sysmoon/go-grpc-example/protos/v1/user"
 )
 
 var Users = []*userpb.UserMessage{
